a8c4778b918808eedf67b03089246da19c62859b
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-12-01 14:05:43 +0800

    fix

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -20,33 +20,23 @@ struct Home: View {
    var body: some View {
       ZStack(alignment: .top) {
          HomeList()
-            .blur(radius: show ? 20 : 0)
-            .scaleEffect(showProfile ? 0.95 : 1)
-            .animation(.default)
-            .background(Color.yellow)
+            .background(Color.red)
+            .padding(.top, 100)
             
-
-         ContentView()
-            .frame(minWidth: 0, maxWidth: 712)
-            .cornerRadius(30)
-            .shadow(radius: 20)
-            .animation(.spring())
-            .offset(y: showProfile ? statusBarHeight + 40 : UIScreen.main.bounds.height)
 
          HStack {
              MenuButton(show: $show)
                 .offset(x: -40)
                  Spacer()
 
-             MenuRight(show: $showProfile)
-                .offset(x: -16)
+
          }
          .offset(y: showProfile ? statusBarHeight : 80)
          .animation(.spring())
 
          MenuView(show: $show)
       }
-      .background(Color("background"))
+      .background(Color.yellow)
       .edgesIgnoringSafeArea(.all)
    }
 }
@@ -55,7 +45,7 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
    static var previews: some View {
       Home()
-         .previewDevice("iPhone X")
+         .previewDevice("iPhone 11")
    }
 }
 #endif
