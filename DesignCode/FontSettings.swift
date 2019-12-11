aa7df70e069bbee527f32fda9d0126d274ba45c0
weiwensangsang <weiwensangsang@users.noreply.github.com>
2019-12-12 00:07:38 +0800

    fix

--- DesignCode/FontSettings.swift
+++ DesignCode/FontSettings.swift
@@ -17,33 +17,18 @@ struct FontSettings: View {
    @State var date = Date()
    @State var email = ""
    @State var submit = false
-
+    var someData = ["a", "b", "c"]
    var body: some View {
       NavigationView {
          Form {
-            Toggle(isOn: $receive) {
-               Text("Recieve Notifications")
-            }
-            Stepper(value: $number, in: 1...10) {
-               Text("\(number) Notification\(number > 1 ? "s" : "") per week")
-            }
+            
             Picker(selection: $selection, label: Text("Favourite course")) {
                Text("SwiftUI").tag(1)
                Text("React").tag(2)
             }
-            DatePicker(selection: $date, label: {
-               Text("Date")
-            })
-            Section(header: Text("Email")) {
-               TextField("Your email: ", text: $email)
-                  .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            Button(action: { self.submit.toggle() }) {
-               Text("Submit")
-            }
-            .alert(isPresented: $submit, content: {
-               Alert(title: Text("Thanks"), message: Text("Email: \(email)"))
-            })
+            
+            Text("you picked: \(someData[picked])")
+
          }
          .navigationBarTitle("字体设置")
       }
@@ -53,7 +38,6 @@ struct FontSettings: View {
 #if DEBUG
 struct Settings_Previews: PreviewProvider {
    static var previews: some View {
-      FontSettings()
-   }
+      FontSettings().environmentObject(CoreData()) }
 }
 #endif
